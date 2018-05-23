ca5c0cb82b9f52e3ab1db37d8c887acf774006e9
Mary-Kate James <marykatj@gmail.com>
2018-05-23 14:03:53 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,5 +1,17 @@
+require 'pry'
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+  first_name = ""
+  data.each do |season_number, information| #information is an array
+    if season_number == season
+      information["status"].each do |stat, outcome|
+        if outcome == "Winner"
+          first_name = information.fetch("name")
+        end
+      end
+    end
+  end
+  first_name
 end
 
 def get_contestant_name(data, occupation)
