f1276b7aecb2324023ca83c5955cce04087c28bc
Mary-Kate James <marykatj@gmail.com>
2018-05-23 14:55:06 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -64,7 +64,7 @@ def get_average_age_for_season(data, season)
   data.each do |season_number, information| #information is an array
       if season_number == season
         information.each do |info_hash|
-              sum = sum + (info_hash["age"].to_i)
+              sum += (info_hash["age"].to_i)
             end
             average = sum/2
             binding.pry
