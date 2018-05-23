5b03cc933f937838cff4c4a3e5aca387a79e4d4f
Mary-Kate James <marykatj@gmail.com>
2018-05-23 14:26:31 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -31,7 +31,17 @@ def get_contestant_name(data, occupation)
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  counter = 0
+  data.each do |season_number, information| #information is an array
+      information.each do |info_hash|
+        info_hash.each do |attribute, outcome|
+          if outcome == hometown
+            counter += 1
+          end
+        end
+    end
+  end
+  counter
 end
 
 def get_occupation(data, hometown)
